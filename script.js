78cfaf5c1e9be85ad0f812589b7a2b1d8cd47a34
Colin-techdev <clarencecolin@gmail.com>
2023-01-16 22:19:46 -0500

    added edits

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementsByClassName("main-items").onmouseover = function() {
+    this.style.color = "red";
+  };
+//   document.getElementsByClassName("main-items").onmouseout = function() {
+//     this.style.color = "black";
+//   };
+  
